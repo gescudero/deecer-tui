96addc478ac42e7aa0711cdc558f77e821dadaf7
Guillermo Escudero <gescudero@gmail.com>
2026-06-25 16:28:20 +0200

    Primer esqueleto de la api para deezer
    La idea es hacer un pequeño esqueleto con el que hacer pruebas de la api mientras voy dandole funcionalidad

---
+++ src/deezer_api.c
@@ -0,0 +1,34 @@
+#include "deezer_api.h"
+
+static size_t writecallback(char *contents, size_t size, size_t nmemb, void *userp) {
+    
+    /*
+     * realsize is alwais size * nmemb
+     * the data is in contents
+     * userp is what CURLOPT_WRITEDATA set, 
+     * can be the structure or object you want
+     *
+     * return realsize
+     */
+    size_t realsize = size * nmemb;
+    struct memory *mem = (struct memory *)userp;
+
+    char *ptr = realloc(mem->memory, mem->size + realsize + 1);
+    if (ptr == NULL){
+        return 0;
+    }
+    mem->memory = ptr;
+    memcpy(&mem->memory[mem->size], contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size]=0;
+    return realsize;
+}
+
+
+
+char* deezer_search(const char *query) {
+    char *resp = "";
+
+
+    return resp;
+}
---
+++ src/deezer_api.h
@@ -0,0 +1,55 @@
+#ifndef DEEZER_API_H
+#define DEEZER_API_H
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <curl/curl.h>
+/****
+ *  LIBCURL
+ ***/
+struct memory {
+    char *memory;
+    size_t size;
+};
+
+/****
+ * DEEZER MODELS
+ ****/
+typedef struct Track Track;
+typedef struct Album Album;
+typedef struct Artist Artist;
+
+struct Track {
+    int id; // id
+    bool readable; // is readable on device
+    char *title; // track title
+    char *title_short; // track short title
+    Artist *artist; // Artist object
+    Album *album; // Album object
+    char *preview; // link to preview [url]
+};
+
+struct Album {
+    int id; // id
+    char *title; // title of album
+    int nb_tracks; // number of tracks
+    Artist *artist; // artist object
+    Track **tracks; // list of track objects
+};
+
+struct Artist {
+    int id; // id
+    char *name; // name
+    char *link; // link to deezer [url]
+    char *tracklist; //api link to top of this artist [url]
+};
+
+char* deezer_search(const char *query);
+char* deezer_get_album(int id);
+char* deezer_get_artist(int id);
+char* deezer_get_chart(int id);
+char* deezer_get_playlist(int id);
+char* deezer_get_track(int id);
+char* deezer_get_user();
+
+#endif
