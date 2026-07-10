f954816e60495db7e0be6cd8d8d25d097bbfc64f
Guillermo Escudero <gescudero@gmail.com>
2026-07-10 18:25:18 +0200

    comenzamos API deezer privada - Vamos creando el cambio del uso de la api pública de deezer, que da   muy poquita cosa, a la api privada con el uso del arl del usuario, con   la que ya podemos conseguir más cosas. - Se han creado algunas estructuras y funciones para comenzar a probar   esa api privada, pero aún no se ponen en práctica. main sigue usando   la api publica.

---
+++ src/deezer_priv_api.h
@@ -0,0 +1,110 @@
+//deezer_priv_api.h
+/******************
+ *
+ * API privada de deezer 
+ * Necesitamos arl para usarla
+ *
+ ******************/
+
+#ifndef DEEZER_PRIV_API_H
+#define DEEZER_PRIV_API_H
+#include "models.h"
+#include <stdbool.h>
+#include <time.h>
+#include <curl/curl.h>
+
+struct memory_t {
+    char * memory;
+    size_t size;
+};
+
+struct deezer_client_t {
+    CURL *curl_handle; // the curl object
+    CURLcode curl_res; // response codes for curl 
+    memory_t mem; // dataholder for curl requests
+    char *arl;//from config file
+    char *session_id;//results:SESSION_ID 
+    char *api_token;//results:checkform
+    char *license_token; //results:USER:OPTIONS:license_token
+};
+
+struct user_t {
+    int id; //results:USER:USER_ID
+    char *name;//results:USER:BLOG_NAME
+    char *email;//results:USER:EMAIL
+    char *lovedtracks_id;//results:USER:LOVEDTRACKS_ID 
+    char *user_token;//results:USER_TOKEN 
+};
+struct track_t {
+    int id; //results:DATA:SNG_ID
+    char *title; //results:DATA:SNG_TITLE
+    char *token; //results:TRACK_TOKEN
+    time_t *token_expire; //results:TRACK_TOKEN_EXPIRE
+    artist_t **artist; //results:ARTISTS ->
+    int nb_artists; // number of artists 
+    album_t *album; //results:ALB_ID ->
+    char *media_url; // peticion a -> DEEZER_MEDIA_URL
+};
+struct artist_t {
+    int id;
+    char *name;
+    track_t **tops;
+    album_t **albums;
+};
+struct album_t {
+    int id;
+    char *title;
+    artist_t **artists;
+    int nb_artists;
+    track_t **tracks;
+    int nb_tracks;
+
+};
+struct playlist_t {
+    int id;
+    char *title;
+    user_t *user;
+    int nb_tracks;
+    track_t **tracks;
+};
+
+// Funcion de inicializacion
+// se encarga de crear todo lo necesario
+// para arrancar el cliente y el pool de objetos
+// solo se debe ejecutar una vez
+int deezer_init(config_t *config);
+
+// constructores
+int deezer_create_client(deezer_client_t *clt);
+int deezer_create_user(user_t *usr);
+track_t *deezer_create_track();
+artist_t *deezer_create_artist();
+album_t *deezer_create_album();
+playlist_t *deezer_create_playlist();
+
+// utilities
+content_t *deezer_search(const char *query);
+
+// getters (pide el objeto a la pool, y si no existe
+// lo pedira a la api)
+user_t *deezer_get_user(int id);
+track_t *deezer_get_track(int id);
+artist_t *deezer_get_artist(int id);
+album_t *deezer_get_album(int id);
+playlist_t *deezer_get_playlist(int id);
+
+// comprobadores
+bool deezer_track_is_valid(track_t *track);
+bool deezer_artist_is_valid(artist_t *artist);
+bool deezer_album_is_valid(album_t *album);
+bool deezer_playlist_is_valid(playlist_t *playlist);
+
+// destructores
+void deezer_free_client(deezer_client_t *client);
+void deezer_free_user(user_t *user);
+void deezer_free_track(track_t *track);
+void deezer_free_artist(artist_t *artist);
+void deezer_free_album(album_t *album);
+void deezer_free_playlist(playlist_t *playlist);
+
+#endif
--- src/models.h
+++ src/models.h
@@ -3,10 +3,24 @@
 #define MODELS_H
 
 typedef struct config_t config_t;
+typedef struct memory_t memory_t;
 typedef struct content_t content_t;
+typedef struct deezer_client_t deezer_client_t;
+typedef struct user_t user_t;
 typedef struct track_t track_t;
 typedef struct album_t album_t;
 typedef struct artist_t artist_t;
 typedef struct playlist_t playlist_t;
+
+// valores de resultados, inspirado en VkResult de VULKAN
+enum deecer_result {
+    DC_SUCCESS = 0,
+    DC_NOT_READY = 1,
+    DC_TIMEOUT = 2,
+    DC_ERROR_INICIALIZATION_FAILED = -1,
+    DC_ERROR_MEMORY_MAP_FAILED = -2,
+    DC_ERROR_UNKNOWN = -5,
+    DC_ERROR_CURL_RESPONSE_ERROR = -6,
+};
 
 #endif
